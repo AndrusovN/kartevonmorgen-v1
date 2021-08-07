af0b1467376df1536e0d182470ae3855d3c7c028
Vanya-afk <wot.vania@mail.ru>
2021-08-07 18:48:25 +0300

    feat: add logo and responsive

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,31 +2,9 @@ import { FC } from 'react'
 import Image from 'next/image'
 import { Button } from 'antd'
 import Link from 'next/link'
+import data from '../utils/co-map/partners.json'
 
 require('../styles/globals.less')
-
-const partnersArr = [
-  {
-    imageURL: '/assets/img/partners_co_map/AIESEC-New-Logo1.png',
-    siteURL: 'https://aiesec.org/',
-    w: '160', h: '170', alt: 'AIESEC',
-  },
-  {
-    imageURL: '/assets/img/partners_co_map/RED-Moscow-Up-and-Running-DG-3.png',
-    siteURL: 'https://impacthub.net/',
-    w: '200', h: '80', alt: 'Impact Hub',
-  },
-  {
-    imageURL: '/assets/img/partners_co_map/gladway.jpg',
-    siteURL: 'http://www.gladway.ru/',
-    w: '500', h: '250', alt: 'Gladway',
-  },
-  {
-    imageURL: '/assets/img/partners_co_map/big-catalog-16212842451.png',
-    siteURL: 'https://xn----htbbc4anafbkmm.xn--p1ai/',
-    w: '200', h: '100', alt: 'Простое Дело',
-  },
-]
 
 const Partners: FC = () => {
   return (
@@ -35,7 +13,7 @@ const Partners: FC = () => {
         Партнёры
       </h1>
       <div className={'partners_container'}>
-        {partnersArr.map(p => {
+        {data.map(p => {
           return <div>
             <Link href={p.siteURL}>
               <a>
