78724e1efd23ae059de8e50efb9e5bdce30d0ecc
AndrusovN <andrusov.n@gmail.com>
2021-08-08 04:01:15 +0300

    fix loader error

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import MapBannerCoMap from '../components/co-map/MapBannerCoMap'
 import { TeamCoMap } from '../components/co-map/TeamCoMap'
 import Partners from '../components/co-map/Partners'
 import HowToUseMap from '../components/co-map/HowToUseMap'
+import HeaderCardContainer from '../components/co-map/HeaderCardContainer'
+import ChooseDirectionSearch from '../components/co-map/ChooseDirectionSearch'
 
 const Main: FC = () => {
 
@@ -12,8 +14,10 @@ const Main: FC = () => {
     <>
         <HomeHeaderCoMap/>
         <MapBannerCoMap/>
+        <HeaderCardContainer/>
         <div style={{display: 'flex', justifyContent: 'center'}}>
           <div className={"bounding_element"}>
+            <ChooseDirectionSearch/>
             <HowToUseMap/>
             <TeamCoMap/>
             <Partners/>
