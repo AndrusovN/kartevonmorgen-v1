40718089c6beb86787e772c40bde163004571ecc
AndrusovN <andrusov.n@gmail.com>
2021-08-08 05:04:55 +0300

    fix build for co-map.ru

--- server.build.production.js
+++ server.build.production.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-process.env.NEXT_PUBLIC_SELF_API = `https://ssr.kartevonmorgen.org/api/v0`
+process.env.NEXT_PUBLIC_SELF_API = `https://co-map.ru/api/v0`
 process.env.DB_NAME = 'kartevonmorgen.sqlite'
 
 
